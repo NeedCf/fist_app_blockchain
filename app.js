233fb42a5c89189a42ae2315cae1c8190e308358
NeedCf <94354224+NeedCf@users.noreply.github.com>
2021-11-16 23:16:05 +0700

    fix error

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // install : cmd => npm install crypto-js
-const hash = request('crypto-js/sha256')
+const hash = require('crypto-js/sha256');
 class Node {
 
     // init NODE
     constructor(prevHash, data) {
-        this.prevHash = prevHash
-        this.data = data
+        this.prevHash = prevHash;
+        this.data = data;
         // time create NODE = NOW
-        this.timeStamp = new Date()
+        this.timeStamp = new Date();
 
-        this.hash = this.caculatorHash()
+        this.hash = this.caculatorHash();
     }
 
     caculatorHash() {
-        return hash(this.prevHash + JSON.stringify(this.data) + this.timeStamp)
+        return hash(this.prevHash + JSON.stringify(this.data) + this.timeStamp).toString();
     }
 }
 
@@ -22,7 +22,7 @@ class BlockChain {
     // init Block Chain
     constructor() {
         // Create start NODE, because last node == null
-        const genesisBlock = new Block('0000', { isGenesis: true })
+        const genesisBlock = new Node('0000', { isGenesis: true });
 
         // Add start NODE to chain
         this.chain = [genesisBlock];
@@ -30,31 +30,31 @@ class BlockChain {
 
     // get last block in chain
     getLastBlock() {
-        return this.chain[this.chain.length - 1]
+        return this.chain[this.chain.length - 1];
     }
 
     // Add a NODE to chain
     addBlock(data) {
         // check last Block
-        const lastBlock = this.getLastBlock()
+        const lastBlock = this.getLastBlock();
         // create Node by last block bash and new Data
-        const newBlock = Node(prevBlock.hash, data)
+        const newBlock = new Node(lastBlock.hash, data);
 
         // add new NODE to last chain
-        this.chain.push(newBlock)
+        this.chain.push(newBlock);
     }
 
     // Check block chain valid
     // if true => block chain unedited 
     // if false => block chain edited
     isValid() {
-        for (i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i]
-            const prevBlock = this.chain[i - 1]
+        for (var i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const prevBlock = this.chain[i - 1];
 
             // Check NODE data edited 
             // if edit DATA and no edit hash => caculatorHash != hash
-            if (currentBlock.caculatorHash != currentBlock.hash) {
+            if (currentBlock.caculatorHash() != currentBlock.hash) {
                 return false;
             }
 
@@ -65,11 +65,23 @@ class BlockChain {
                 return false;
             }
         }
-        return true
+        return true;
     }
 }
 
 TuChain = new BlockChain()
+
 TuChain.addBlock(
-    data({ from: "Tu", to: "Thu", amount: "1000" })
+    ({ from: "Tu", to: "Thu", amount: "8600" })
 )
+
+TuChain.addBlock(
+    ({ from: "Thu", to: "Truc", amount: "100" })
+)
+
+TuChain.addBlock(
+    ({ from: "Truc", to: "Tu", amount: "8900" })
+)
+
+console.log(TuChain.chain)
+console.log(TuChain.isValid())
